5215012d0f520e4ca27bf3b8ab00a42b03ffa2e4
ishuks9 <ishuks9@gmail.com>
2021-08-30 11:45:17 +0530

    updated UC1

---
+++ IndianCensusCsv.java
@@ -0,0 +1,13 @@
+public class IndianCensusCSV {
+	String state;
+	int population;
+	int areaInSqKm;
+	int densityPerSqKm;
+	
+	@Override
+	public String toString() {
+		return "IndianCensusCSV [state=" + state + ", population=" + population + ", areaInSqKm=" + areaInSqKm
+				+ ", densityPerSqKm=" + densityPerSqKm + "]";
+	}
+	
+}
